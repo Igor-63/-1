24e6b75250af67321c71d85af4d52d6826d6eeb7
Igor-63 <werati@rambler.ru>
2024-09-06 21:10:29 +0400

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -4,8 +4,28 @@ def get_multiplied_digits(number):
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
 
+#    else:
+#       return first
+
+# нечеткая постановка задачи.
+# из примера результата следует, что нули не учитываются,
+# при этом, если  нуль последний, то в этом варианте будет умножение на нуль и результат будет равен нулю
+    
+
+
     else:
-        return first
+        if number != 0:  
+            return first
+        else:
+            return 1
+
+
+# вариант с учетом исключения последнего нуля. Умножаются только цифры больше нуля
+
+
+
+
+
 
 
 result = get_multiplied_digits(402034)
