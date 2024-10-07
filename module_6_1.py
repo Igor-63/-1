144bf328463445929eb5baff72a4adf75e9157f8
Igor-63 <werati@rambler.ru>
2024-10-07 14:32:44 +0400

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,68 @@
+class Animal:
+    # животные
+
+    def __init__(self, name):
+        self.name = name
+
+    alive = True # живой
+    fed = False # накормленный
+    name = ''  # название
+
+class Plant:
+    # растение
+
+    def __init__(self, name):
+        self.name = name
+
+    edible = False #съедобность
+    name = '' # название
+
+class Mammal(Animal):
+    # травоядное
+
+
+    def eat (self, food):
+         if food.edible == True:
+            print(f'{self.name} съел {food.name}' )
+            self.fed = True
+         else:
+            print(f'{self.name} не стал есть  {food.name}')
+            self.alive = False
+
+class Predator(Animal):
+    # хищник
+
+    def eat(self, food):
+        if food.edible == True:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть  {food.name}')
+            self.alive = False
+
+class Flower(Plant):
+    # цветок
+    pass
+
+class Fruit(Plant):
+    def __init__(self, name):
+        self.name = name
+        self.edible = True
+
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+
+print(a1.name)
+print(p1.name)
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
+
