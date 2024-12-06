a92d21aa82ea88c9259ea7c42bfcadf3558dedc1
Igor-63 <werati@rambler.ru>
2024-12-06 16:10:22 +0400

    Update module_10_5.py

--- module_10_5.py
+++ module_10_5.py
@@ -1,9 +1,11 @@
-import time, threading, multiprocessing
+import datetime
+import multiprocessing
+import time
 
 
 def read_info(name):
     all_data = []
-    my_file1 = open(name, 'r', encoding='utf-8' )
+    my_file1 = open(name, 'r', encoding='utf-8')
     non_void_line = True
     while non_void_line:
         non_void_line = my_file1.readline()
@@ -12,10 +14,6 @@ def read_info(name):
 
 
 filenames = [f'./file {number}.txt' for number in range(1, 5)]
-
-
-
-
 
 n_proc = 4
 
@@ -27,23 +25,15 @@ if __name__ == "__main__":
         read_info(my_file)
     time2 = time.time()
 
-    print(f'{time2 - time1} (линейный)')
+    print(f'{datetime.timedelta(seconds=time2 - time1)} (линейный)')
 
     time1 = time.time()
 
     pool = multiprocessing.Pool(processes=n_proc)
     pool.map(read_info, filenames)
-    # pool.close()
-    # pool.join()
+    pool.close()
+    pool.join()
 
     time2 = time.time()
 
-    print(f'{time2-time1}  (многопроцессный)')
-
-
-
-
-
-
-
-
+    print(f'{datetime.timedelta(seconds=time2 - time1)}  (многопроцессный)')
